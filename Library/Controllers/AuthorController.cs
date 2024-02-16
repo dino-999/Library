08c0ffbbe3257e969aca52438db992636106e870
dinojagar <dino.jagar@gmail.com>
2024-02-16 17:49:43 +0100

    #43

--- Library/Controllers/AuthorController.cs
+++ Library/Controllers/AuthorController.cs
@@ -13,6 +13,124 @@ namespace Library.Controllers
     [ApiController]
     public class AuthorController : ControllerBase
     {
- 
+        private readonly DatabaseContext _context;
+
+        public AuthorController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuthorDTO>>> GetAuthors()
+        {
+            var authors = await _context.Authors
+                .Include(author => author.Books)
+                .ToListAsync();
+
+            var authorDTOs = authors.Select(author => new AuthorDTO
+            {
+                AuthorId = author.AuthorId,
+                FirstName = author.FirstName,
+                LastName = author.LastName,
+                DateOfBirth = author.DateOfBirth,
+                Title = author.Title,
+                Books = author.Books.Select(book => new BookDTO
+                {
+                    BookId = book.BookId,
+                    Title = book.Title,
+                    Year = book.Year
+                }).ToList()
+            }).ToList();
+
+            return authorDTOs;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuthorDTO>> GetAuthor(int id)
+        {
+            var author = await _context.Authors
+                .Include(a => a.Books)
+                .FirstOrDefaultAsync(a => a.AuthorId == id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            var authorDTO = new AuthorDTO
+            {
+                AuthorId = author.AuthorId,
+                FirstName = author.FirstName,
+                LastName = author.LastName,
+                DateOfBirth = author.DateOfBirth,
+                Title = author.Title,
+                Books = author.Books.Select(book => new BookDTO
+                {
+                    BookId = book.BookId,
+                    Title = book.Title,
+                    Year = book.Year
+                }).ToList()
+            };
+
+            return authorDTO;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Author>> PostAuthor(Author author)
+        {
+            _context.Authors.Add(author);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAuthor", new { id = author.AuthorId }, author);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuthor(int id, Author author)
+        {
+            if (id != author.AuthorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.AuthorId == id);
+        }
     }
 }
