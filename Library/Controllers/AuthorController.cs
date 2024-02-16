0626f62cf8ef8276c79f81ae4aee6759e3c07e2c
dinojagar <dino.jagar@gmail.com>
2024-02-16 17:49:26 +0100

    #41

---
+++ Library/Controllers/AuthorController.cs
@@ -0,0 +1,18 @@
+using Library.Database;
+using Library.DTOs;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+ 
+    }
+}
